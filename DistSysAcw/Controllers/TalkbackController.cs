47d95207224bb373d71a410fe0464ab7854cdb83
Ștefan Beregszaszy <stefan.beregszaszy@gmail.com>
2021-03-29 15:18:23 +0300

    Am dat upload la copia descarcata de proiect. Ma iau de TASK 1 si 2, si vad cum iese, cu Dumnezeu inainte.
      TASK 1 terminat.( in afara de coduri de eroare)

--- DistSysAcw/Controllers/TalkbackController.cs
+++ DistSysAcw/Controllers/TalkbackController.cs
@@ -1,15 +1,40 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DistSysAcw.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     public class TalkbackController : BaseController
     {
         /// <summary>
         /// Constructs a TalkBack controller, taking the UserContext through dependency injection
         /// </summary>
         /// <param name="context">DbContext set as a service in Startup.cs and dependency injected</param>
-        public TalkbackController(Models.UserContext dbcontext) : base(dbcontext) { }
+        /// 
+        
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+        public TalkbackController(Models.UserContext dbcontext) : base(dbcontext)
+        {
+            
+        }
+
+        
+        [ActionName("Hello")]
+        [HttpGet]
+        public string hello()
+        {
+            #region TASK1
+            // TODO: add api/talkback/hello response
+            return "Hello World";
+            #endregion
+        }
 
         //[ActionName("Hello")]
         //public string Get()
@@ -18,6 +43,42 @@ namespace DistSysAcw.Controllers
         //    // TODO: add api/talkback/hello response
         //    #endregion
         //}
+
+        [ActionName("Sort")]
+        [HttpGet]
+        public string sort([FromQuery]int[] integers)
+        {
+            #region TASK1
+
+            string message = "";
+
+            Array.Sort(integers);
+
+            foreach(var num in integers)
+            {
+                if (num != integers.Last<int>())
+                {
+                    message = message + num.ToString() + ",";
+                }
+                else
+                {
+                    message = message + num.ToString();
+                }
+                
+            }
+
+            //TODO:
+            //   add a parameter to get integers from the URI query
+            //   sort the integers into ascending order
+            //   send the integers back as the api/talkback/sort response
+            //   conform to the error handling requirements in the spec
+
+                return "[" + message + "]"; 
+
+           
+
+            #endregion
+        }
 
         //[ActionName("Sort")]
         //    #region TASK1
